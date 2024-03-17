e4d008ab5f89ce4ae60c785c0febb3fe13703c1c
willychen0146 <willychen0146@gmail.com>
2024-03-17 14:16:43 +0800

    feat: add login button and import web font

--- src/app.js
+++ src/app.js
@@ -7,3 +7,10 @@ const menuList = $('#menuList');
 menuButton.addEventListener('click', function() {
     menuList.classList.toggle('hidden');
 });
+
+const logInButton = $('#logInButton');
+const logInEntrance = $('#logInEntrance');
+
+logInButton.addEventListener('click', function() {
+    logInEntrance.classList.toggle('hidden');
+});
